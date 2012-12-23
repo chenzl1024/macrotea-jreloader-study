8b5f0cc9d8a9b6d2acec8facc2aba1a022b58c4a
macrotea <macrotea@qq.com>
2012-12-23 16:16:51 +0800

    添加说明注释
    Signed-off-by: macrotea <macrotea@qq.com>

--- src/test/java/com/mtea/macrotea_jreloader_study/SysPrinter.java
+++ src/test/java/com/mtea/macrotea_jreloader_study/SysPrinter.java
@@ -10,8 +10,21 @@ package com.mtea.macrotea_jreloader_study;
 public class SysPrinter {
 	
 	public void doPrint(){
+		//在线程运行期间,您可以在这里随时添加内容,测试是否能够热部署
 		System.out.println("test print - 1");
 		System.out.println("test print - 2");
+		//System.out.println("test print - 3");
+		//System.out.println("test print - 4");
+		
+		//当添加已有方法则起效,如showMe()
+		showMe();
+		
+		//若新增方法,且在doPrint()方法体重运行则有问题
+		//newMethod();
+	}
+	
+	public void showMe(){
+		System.out.println("macrotea@qq.com");
 	}
 
 }
